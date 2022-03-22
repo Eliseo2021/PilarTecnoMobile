e4c7734dc9fadfe907069115f2314833aab70458
Eliseo2021 <prof.lic.eliseo.acevedo@gmail.com>
2022-03-22 15:03:28 -0300

    4-> *En la pantalla de perfil incluir - datos de usuario

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,3 +1,4 @@
+/*
 import React, { Component, useCallback } from "react";
 import { Button, SafeAreaView, View, Text, ImageBackground } from "react-native";
 
@@ -13,4 +14,69 @@ function Profile({ navigation }) {
     </View>
   );
 }
+export default Profile;
+*/
+
+import React, { useState } from 'react';
+import {
+    SafeAreaView,
+    View,
+    Text,
+    Image,
+    TouchableOpacity,
+} from 'react-native';
+import { Input, Icon, Button } from 'react-native-elements';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { actions } from '../../store/actions';
+import { useDispatch } from 'react-redux';
+
+import Header from '../../components/Header';
+import { styles } from './styles';
+
+
+const Profile = () => {
+
+    const dispatch = useDispatch()
+
+    const _sigOut = async () => {
+        try {
+            await AsyncStorage.setItem('user', JSON.stringify(false))
+        } catch (e) {
+            // saving error
+        }
+        dispatch(actions.user.setUser(false))
+    }
+
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <Header />
+            <View style={{ flexDirection: 'column' }}>
+                <View style={styles.header}></View>
+                <Image
+                    style={styles.avatar}
+                    source={{ uri: 'https://randomuser.me/api/portraits/men/36.jpg' }}
+                />
+                <View style={styles.body}>
+                    <Text style={styles.name}>Juan Carlos</Text>
+                    <Text style={styles.info}>UX Designer / Mobile developer</Text>
+                    <Text style={styles.description}>
+                        DESCRIPCION Lorem ipsum dolor sit amet, saepe sapientem eu nam.
+                    </Text>
+                    <TouchableOpacity style={styles.buttonContainer}>
+                        <Text>Pilar Tecno </Text>
+                    </TouchableOpacity>
+                    <Button style={styles.buttonSingOut}
+                        title="SALIR"
+                        onPress={() => _sigOut()}
+                        containerStyle={{ width: '80%' }}
+                    />
+                </View>
+
+            </View>
+        </SafeAreaView>
+    )
+}
+
+
 export default Profile;
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -1,20 +1,74 @@
-import { StyleSheet, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
 
-const width = Dimensions.get("window").width;
-const height = Dimensions.get("window").height;
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#292929",
+    justifyContent: 'center',
+    alignItems: 'center',
+  }, header: {
+    backgroundColor: '#00BFFF',
+    height: 200,
   },
-  mainContent: {
-    flex: 1,
-    width,
-    height,
-    padding: 5,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: 'white',
+    marginBottom: 10,
+    alignSelf: 'center',
+    position: 'absolute',
+    marginTop: 130,
   },
-});
+  body: {
+    marginTop: 40,
+    alignItems: 'center',
+    padding: 30,
+  },
+  name: {
+    fontSize: 28,
+    color: '#696969',
+    fontWeight: '600',
+  },
+  info: {
+    fontSize: 16,
+    color: "#00BFFF",
+    marginTop: 10,
+  },
+  description: {
+    fontSize: 16,
+    color: "#696969",
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    marginTop: 10,
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 50,
+    width: 250,
+    borderRadius: 30,
+    backgroundColor: '#00BFFF',
+  },
+    buttonSingOut: {
+    marginTop: 50,
+    fontSize: 22,
+    height: 45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: 250,
+    borderRadius: 5,
+    backgroundColor: '#00BFFF',
+  },
+})
+
