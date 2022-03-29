d96415071cdd81d702463753b19b92a2bac50d9d
Eliseo2021 <prof.lic.eliseo.acevedo@gmail.com>
2022-03-29 09:31:14 -0300

    3-> * listado de recursos mediante get

--- src/screens/List/index.js
+++ src/screens/List/index.js
@@ -1,4 +1,4 @@
-import React, { Component, useCallback } from "react";
+/* import React, { Component, useCallback } from "react";
 import { Button, SafeAreaView, View, Text, ImageBackground } from "react-native";
 
 import { styles } from "./styles";
@@ -9,8 +9,68 @@ function List({ navigation }) {
     <View  style={styles.container}>
 <Header />
       <Text  style={styles.mainContainer}>Lista screen</Text>
-      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
+export default List; */
+
+import React, { useState, useEffect } from "react";
+import { View, FlatList, TouchableOpacity } from "react-native";
+import { ListItem, Avatar} from "react-native-elements";
+
+import { styles } from "./styles";
+
+import Header from "../../components/Header";
+import { getPokemonList, IMG_URL } from "../../api";
+import { getPokemonImgId } from "../../utils";
+
+function List(props) {
+  const [pokemons, setPokemons] = useState();
+  const [next, setNext] = useState();
+
+  useEffect(() => {
+    getPokemonList().then((data) => {
+      //console.log('data :' + JSON.stringify(data))
+      setPokemons(data.results);
+      console.log(setPokemons);
+      setNext(data.next);
+    });
+  }, []);
+
+  const renderItem = (item) => {
+    //console.log('data :' + JSON.stringify(item))
+    const path = item.url.split("/");
+    const imgID = getPokemonImgId(path[6]);
+
+    return (
+      <TouchableOpacity
+        onPress={() => props.navigation.navigate("", (item = { item }))}
+        style={styles.containerTouchable}
+      >
+        <ListItem bottomDivider style={styles.containerBottomDivider}>
+          <ListItem.Content>
+            <ListItem.Title>{item.name}</ListItem.Title>
+            <ListItem.Subtitle>{item.id}</ListItem.Subtitle>
+          </ListItem.Content>
+          <Avatar source={{ uri: `${IMG_URL}${imgID}.png` }} />
+        </ListItem>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.mainContainer}>
+        <FlatList
+          data={pokemons}
+          renderItem={(item, index) => renderItem(item.item, index)}
+          keyExtractor={(item, index) => index}
+          style={styles.containerFlatList}
+        />
+      </View>
+    </View>
+  );
+}
+
 export default List;
--- src/screens/List/styles.js
+++ src/screens/List/styles.js
@@ -7,6 +7,9 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#292929",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
   },
   mainContent: {
     flex: 1,
@@ -16,5 +19,21 @@ export const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#292929",
+  },
+  containerTouchable: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: -40,
+    marginVertical: "2%",
+    borderWidth: 0.5,
+    borderColor: "#707070",
+    borderRadius: 5,
+  },
+  containerBottomDivider: {
+    width,
+  },
+  containerFlatList: {
+    marginTop: height / 10,
   },
 });
