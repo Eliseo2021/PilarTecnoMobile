7895e6fde9a0a58e11fd4ca608042f3d84cb0e47
Eliseo2021 <prof.lic.eliseo.acevedo@gmail.com>
2022-04-07 21:57:45 -0300

    3-> * Autenticación escucha activa de sesión

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import 'react-native-gesture-handler';
-import React, { Component, useCallback } from 'react';
+import React, { Component, useCallback, useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
-import {Provider} from 'react-redux';
-import { store } from '../store';
+import { useDispatch, Provider} from 'react-redux';
+import { store, actions } from '../store';
+import auth from '@react-native-firebase/auth';
 
 import AppStack from '../routs/app';
 
@@ -13,8 +14,33 @@ export default App = () => {
     return (
 	<Provider store={store}>
         <NavigationContainer>
-            <AppStack />
+           <AppWrapped />
         </NavigationContainer>
 	</Provider>
     )
+}
+
+const AppWrapped = () => {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+  const dispatch = useDispatch()
+  // Handle user state changes
+  async function onAuthStateChanged(user) {
+  if(user){
+  setUser(user)
+  }else{
+  dispatch(actions.user.setUser(null))
+  }
+  if (initializing) setInitializing(false);
+  }
+  useEffect(() => {
+  const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+  return subscriber; // unsubscribe on unmount
+}, []);
+if (initializing){ return null;}
+return(
+
+<AppStack />
+
+);
 }
